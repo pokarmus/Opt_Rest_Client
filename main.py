5e93ac99b99c00e1df8ea7eae5a843fdeece43c1
pawel.okarmus <pokarmus@o2.pl>
2022-02-10 15:10:10 +0100

    clearing the code

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ if __name__ == '__main__':
 
     path = read_con_params()["inputDir"]
 
-    while (True):
+    while True:
         files_list = list_files()
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         print("Lista plików w katalogu wejściowym: " + str(files_list))
-        if len(files_list)>0:
+        if len(files_list) > 0:
             print("Pobieram plik " + files_list[0])
             try:
                 session = login_from_file()
-                response = send_vrp_json(session, path +"/" + files_list[0])
-                dir = create_dir(path +"/" + files_list[0])
-                move_file_to_output(path +"/" + files_list[0])
-                save_resoult_to_output(response, dir, path +"/" + files_list[0])
+                response = send_vrp_json(session, path + "/" + files_list[0])
+                dir_f = create_dir(path + "/" + files_list[0])
+                move_file_to_output(path + "/" + files_list[0])
+                save_resoult_to_output(response, dir_f, path + "/" + files_list[0])
             except ValueError:
                 print("!!! Błędny plik, sprawdź zawartość i spróbuj ponownie !!!")
-                dir = create_dir(path + "/" + files_list[0])
+                dir_f = create_dir(path + "/" + files_list[0])
                 move_file_to_output_err(path + "/" + files_list[0])
             except requests.exceptions.RequestException:
                 print("!!! Nie udało się połączyć z serwerem. Nastąpi ponowna próba połączenia !!!")
